cd51778f29228b7257ac8ed7bd3bdbcc5faa5574
Ismayil44 <abdullayevismayil88@gmail.com>
2025-02-15 21:57:03 +0400

    Update Main.java

--- happy-family-v2/src/Main.java
+++ happy-family-v2/src/Main.java
@@ -12,36 +12,36 @@ public class Main {
 
         Family FirstFamily=new Family(FirstFamilyMother,FirstFamilyFather);
 
-        Pet SecondChildPet=new Pet("dog","Rock",5,75,habits,FirstFamily);
+        Pet pet=new Pet("dog","Rock",5,75,habits);
 
 
         Human FirstFamilyChild1=new Human("Michael","Karleone",1977);
 
         String[][] schedule = {{"5","7"},{"Resting","Hanging out"}};
-        Human FirstFamilyChild2=new Human("Antonio","Karleone",1978,98,schedule);
+        Human FirstFamilyChild2=new Human("Antonio","Karleone",1978,98,schedule,FirstFamily);
+
+
         FirstFamily.addChild(FirstFamilyChild2);
         FirstFamily.addChild(FirstFamilyChild1);
-        FirstFamily.addPet(SecondChildPet);
-        System.out.println(FirstFamily);
-        FirstFamily.countOfFamilyMembers();
-        FirstFamily.deleteChild(0);
+        FirstFamily.addPet(pet);
         System.out.println(FirstFamily);
         FirstFamily.countOfFamilyMembers();
 
-        //System.out.println("The family members\n" + FirstFamilyMother);
-        //System.out.println(FirstFamilyFather);
-        //System.out.println(FirstFamilyChild1);
-        //System.out.println(FirstFamilyChild2.toString());
+
+        FirstFamily.deleteChildV2(FirstFamilyChild2);//FirstFamily.deleteChild(0);
+        System.out.println(FirstFamily);
+        FirstFamily.countOfFamilyMembers();
 
 
-        //FirstFamilyChild2.greetPet();
-        //FirstFamilyChild2.describePet();
+        //actions
+        FirstFamilyChild2.feedPet();
+        FirstFamilyChild2.greetPet();
+        FirstFamilyChild2.describePet();
+        pet.foul();
+        pet.eat();
+        pet.respond();
 
-        //SecondChildPet.eat();
-        //SecondChildPet.foul();
-        //SecondChildPet.respond();
 
 
-        //FirstFamilyChild2.feedPet();
     }
 }
